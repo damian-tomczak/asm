bd43ded084d63e3ac83e02458bc46fd4f94313e4
Damian Tomczak <contact@damian-tomczak.pl>
2022-07-31 06:44:06 +0200

    A look at the project.

--- src.asm
+++ src.asm
@@ -8,15 +8,47 @@ JetXPos byte
 JetYPos byte
 BomberXPos byte
 BomberYPos byte
-    seg Code
-    org $F000
+JetSpritePtr word
+JetColorPtr word
+BomberSpritePtr word
+BomberColorPtr word
+JET_HEIGHT = 9
+BOMBER_HEIGHT = 9
 
+    seg Code
+    org $FF00
+
+    include "assets.h"
 Reset:
     CLEAN_START
     lda #10
     sta JetYPos
     lda #60
     sta JetXPos
+    lda #83
+    sta BomberYPos
+    lda #54
+    sta BomberXPos
+
+    lda #<JetSprite
+    sta JetSpritePtr
+    lda #>JetSprite
+    sta JetSpritePtr+1
+
+    lda #<JetColor
+    sta JetColorPtr
+    lda #>JetColor
+    sta JetColorPtr+1
+
+    lda #<BomberSprite
+    sta BomberSpritePtr
+    lda #>BomberSprite
+    sta BomberSpritePtr+1
+
+    lda #<BomberColor
+    sta BomberColorPtr
+    lda #>BomberColor
+    sta BomberColorPtr+1
 
 StartFrame:
     lda #2
@@ -46,9 +78,40 @@ GameVisibleLine:
     lda #0
     sta PF2
 
-    ldx #192
+    ldx #96
 .GameLineLoop:
+.AreWeInsideJetSprite:
+    txa
+    sec
+    sbc JetYPos
+    cmp JET_HEIGHT
+    bcc .DrawSpriteP0
+    lda #0
+.DrawSpriteP0
+    tay
+    lda (JetSpritePtr),Y
     sta WSYNC
+    sta GRP0
+    lda (JetColorPtr),Y
+    sta COLUP0
+
+.AreWeInsideBomberSprite
+    txa
+    sec
+    sbc BomberYPos
+    cmp BOMBER_HEIGHT
+    bcc .DrawSpriteP1
+    lda #0
+.DrawSpriteP1
+    tay
+    lda #%00000101
+    sta NUSIZ1
+    lda (BomberSpritePtr),Y
+    sta WSYNC
+    sta GRP1
+    lda (BomberColorPtr),Y
+    sta COLUP1
+
     dex
     bne .GameLineLoop
 
